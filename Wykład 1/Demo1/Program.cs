c78c83c881b7f8c8867c3fff397b0609e6dbe4b1
Karpfly <karpfly@gmail.com>
2020-12-08 07:18:02 +0100

    Wykład 1

---
+++ Wykład 1/Demo1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("Podaj imie i numer albumu: ");
+			var imie = Console.ReadLine();
+			Console.WriteLine("Witaj: "+imie);
+        }
+    }
+}
---
+++ Wykład 1/Zadanie domowe/ZadanieDomowe1/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let Greeting name = printfn "Hello %s from F#!" name
+[<EntryPoint>]
+let main argv =
+    Greeting "Maciej"
+    0 // return an integer exit code
