8ca5df46f6bedd81e0a6b645d90c0345ec57dfe8
Karpfly <karpfly@gmail.com>
2020-12-27 12:24:17 +0100

    Zestaw 4 - Zadanie 2

---
+++ Zestaw 4/Zadanie 2/Para.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Zadanie_2
+{
+    public class Para<T> : IComparable<Para<T>> where T : IComparable<T>
+    {
+        private T first = default(T), second = default(T);
+
+        public Para(T first, T second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public override string ToString()
+        {
+            return first.ToString() + "\t" + second.ToString();
+        }
+
+        public int CompareTo(Para<T> other)
+        {
+            if (this.first.Equals(other.first))
+            {
+                return this.second.CompareTo(other.second);
+            }
+            else
+            {
+                return this.first.CompareTo(other.first);
+            }
+        }
+    }
+}
---
+++ Zestaw 4/Zadanie 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Laboratorium 4, zadanie 2");
+            Console.WriteLine("Karpiński Maciej, numer albumu: 39446");
+            Console.WriteLine("Huawei Matebook D 15 (2018)\n");
+
+            Para<string>[] pstr = new Para<string>[7];
+            pstr[0] = new Para<string>("Ola", "Olowska");
+            pstr[1] = new Para<string>("Uta", "Utowska");
+            pstr[2] = new Para<string>("Web", "Webowski");
+            pstr[3] = new Para<string>("Ewa", "Ebowska");
+            pstr[4] = new Para<string>("Jaś", "Jasiński");
+            pstr[5] = new Para<string>("Jaś", "Janowicz");
+            pstr[6] = new Para<string>("Ola", "Olowicz");
+
+            Console.WriteLine("\nPara<String> przed sortowaniem");
+            foreach (var para in pstr)
+            {
+                Console.WriteLine($"\t{para.ToString()}");
+            }
+
+            Array.Sort(pstr);
+            Console.WriteLine("\nPara<String> po sortowaniu");
+            foreach (var para in pstr)
+            {
+                Console.WriteLine($"\t{para.ToString()}");
+            }
+
+        }
+    }
+}
