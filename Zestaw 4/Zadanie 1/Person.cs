c67fe47bbc51bb99987f2306f738c169833498ec
Karpfly <karpfly@gmail.com>
2020-12-27 12:24:06 +0100

    Zestaw 4 - Zadanie 1

---
+++ Zestaw 4/Zadanie 1/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    public class Person : IComparable
+    {
+        private string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Person otherPerson = (Person)obj;
+            if (this.Age == otherPerson.Age)
+            {
+                return this.Name.CompareTo(otherPerson.Name);
+            }
+            else
+            {
+                return this.Age.CompareTo(otherPerson.Age);
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Name + ", wiek: " + this.Age;
+        }
+    }
+}
---
+++ Zestaw 4/Zadanie 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Laboratorium 4, zadanie 1");
+            Console.WriteLine("Karpiński Maciej, numer albumu: 39446");
+            Console.WriteLine("Huawei Matebook D 15 (2018)\n");
+
+            List<Person> lista = new List<Person>();
+            lista.Add(new Person("Anna", 43));
+            lista.Add(new Person("Tomasz", 55));
+            lista.Add(new Person("Magdalena", 18));
+            lista.Add(new Person("Anna", 21));
+            lista.Add(new Person("Justyna", 21));
+            lista.Add(new Person("Stanisław", 55));
+            lista.Add(new Person("Justyna", 19));
+
+            Console.WriteLine("Lista przed sortowaniem: ");
+            foreach (var person in lista)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+            lista.Sort();
+            Console.WriteLine("\nLista po sortowaniu: ");
+            foreach (var person in lista)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
