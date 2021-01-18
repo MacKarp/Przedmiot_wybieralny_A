bab194aedc491ab6ddb7e3494901267d508fe354
Karpfly <karpfly@gmail.com>
2021-01-18 13:55:11 +0100

    Zestaw 5 Zadanie 5

---
+++ Zestaw 5/Zadanie 5/CarDealer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zadanie_5
+{
+    internal class CarDealer
+    {
+        public event EventHandler<CarInfoEventArgs> NewCarInfo;
+
+        public void RaiseNewCarInfo(string car)
+        {
+            Console.WriteLine("\nDealer, nowe auto: {0}", car);
+            NewCarInfo?.Invoke(this, new CarInfoEventArgs(car));
+        }
+    }
+}
