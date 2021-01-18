d5475a2364a6dd4a306df64287ce0d925a5f1d38
Karpfly <karpfly@gmail.com>
2021-01-18 13:54:29 +0100

    Zestaw 5 Zadanie 2

---
+++ Zestaw 5/Zadanie 2/MathOp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zadanie_2
+{
+
+    public class MathOp
+    {
+        public double Add(double x, double y) => x + y;
+        public double Sub(double x, double y) => x - y;
+        public double Mul(double x, double y) => x * y;
+
+        public double Div(double x, double y)
+        {
+            try
+            {
+                return x / y;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Nie można dzielić przez 0!");
+                throw e;
+            }
+        }
+    }
+}
